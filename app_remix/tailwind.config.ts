5c0a32b54520866c4a737dfc7e29450cdd1c303e
Anna <aniemildimitrova@alumnos.cei.es>
2023-07-15 02:38:12 +0200

    15.07

--- app_remix/tailwind.config.ts
+++ app_remix/tailwind.config.ts
@@ -5,7 +5,10 @@ export default {
   theme: {
     extend: {
       colors: {
-            'Gainsboro': '#EEF0F4',
+            'Gainsboro': '#EEF0F4', //Blanco roto
+            'Mint Green': '#A0F280', //Verde Menta
+            'Slate Gray': '#7E8591', //Gris Pizarra
+            'Midnight Blue' : '#111C31' //Azul Medianoche
             }
           },
   },
