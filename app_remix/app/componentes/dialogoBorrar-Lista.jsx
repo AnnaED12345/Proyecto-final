7a13d06e0fb409b7a69e1bffa628660cbfc9f59d
Anna <aniemildimitrova@alumnos.cei.es>
2023-07-20 15:54:52 +0200

    07.20

--- app_remix/app/componentes/dialogoBorrar-Lista.jsx
+++ app_remix/app/componentes/dialogoBorrar-Lista.jsx
@@ -43,49 +43,50 @@ export default function DialogoBorrarLista ({usuarioId, idList, cargarListas, mo
     location.reload();
   };
     
-    const onBorrarListaHandle = (() => { //Acepta la eliminación de la lista y cierra la ventana modal
-        borrarLista();        
-    });
+    const onBorrarListaHandle = () => {
+      //Acepta la eliminación de la lista y cierra la ventana modal
+      borrarLista();
+    };
 
     const onCancelarBorrarLista = () => { //Cancela la eliminación de la lista y cierra la ventana modal
       setModalBorrarLista(false);
       idList = {};
     };
 
-return (
-  <overlay>
-    {modalBorrarLista && (
-      <div className="fixed z-50 inset-0 bg-gray-800 bg-opacity-50 flex items-center justify-center">
-        <modal className="bg-white mx-5 sm:mx-auto rounded-3xl shadow-lg z-50 overflow-y-auto py-10 px-10">
-          <div className="flex items-center justify-center mb-5">
-            <FontAwesomeIcon
-              className="text-SlateGray text-3xl"
-              icon={faTriangleExclamation}
-            />
-          </div>
-          <p className="text-MidnightBlue text-lg font-bold text-center">
-            ¿Estás seguro de que deseas borrar la lista?
-          </p>
-          <p className="text-MidnightBlue text-center text-md font-light">
-            Atención: Se borrarán todas las tareas vinculadas a esta lista
-          </p>
-          <div className="grid grid-cols-2 gap-3 mt-6">
-            <button
-              className="bg-Gainsboro hover:bg-MintGreen text-gray-800 font-thin md:text-lg py-2 rounded-md"
-              onClick={onBorrarListaHandle}
-            >
-              Aceptar
-            </button>
-            <button
-              className="bg-Gainsboro hover:bg-BurntSienna text-gray-800 font-thin md:text-lg py-2 rounded-md"
-              onClick={onCancelarBorrarLista}
-            >
-              Cancelar
-            </button>
-          </div>
-        </modal>
-      </div>
-    )}
-  </overlay>
-);
+  return (
+    <overlay>
+      {modalBorrarLista && (
+        <div className="fixed z-50 inset-0 bg-gray-800 bg-opacity-50 flex items-center justify-center">
+          <modal className="bg-white mx-5 sm:mx-auto rounded-3xl shadow-lg z-50 overflow-y-auto py-10 px-10">
+            <div className="flex items-center justify-center mb-5">
+              <FontAwesomeIcon
+                className="text-SlateGray text-3xl"
+                icon={faTriangleExclamation}
+              />
+            </div>
+            <p className="text-MidnightBlue text-lg font-bold text-center">
+              ¿Estás seguro de que deseas borrar la lista?
+            </p>
+            <p className="text-MidnightBlue text-center text-md font-light">
+              Atención: Se borrarán todas las tareas vinculadas a esta lista
+            </p>
+            <div className="grid grid-cols-2 gap-3 mt-6">
+              <button
+                className="bg-Gainsboro hover:bg-MintGreen text-gray-800 font-thin md:text-lg py-2 rounded-md"
+                onClick={onBorrarListaHandle}
+              >
+                Aceptar
+              </button>
+              <button
+                className="bg-Gainsboro hover:bg-BurntSienna text-gray-800 font-thin md:text-lg py-2 rounded-md"
+                onClick={onCancelarBorrarLista}
+              >
+                Cancelar
+              </button>
+            </div>
+          </modal>
+        </div>
+      )}
+    </overlay>
+  );
   }
